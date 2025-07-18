79a642dce1dd9ccdc1b37e95bf08f0c83e345345
Patyhelenaa <patyhelena160@gmail.com>
2025-07-18 02:32:53 -0300

    feat(api): configura uso de variáveis de ambiente

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000', // ajuste para o endereço do backend no Docker depois
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8000',
 });
 
 // Adiciona o token de autenticação em todas as requisições, exceto login
